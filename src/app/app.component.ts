6b744c6c957449b77cf0a4fa549f5cb357206b6a
Théo Jorge Oliveira <theo.joliveira@gmail.com>
2025-02-19 09:12:52 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { MatToolbar } from '@angular/material/toolbar';
 import { DummyComponent } from './component/dummy/dummy.component';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'crm-root',
-  imports: [RouterOutlet, LoginComponent, MatToolbar, DummyComponent, RouterLink, RouterLinkActive, MatIcon],
+  imports: [RouterOutlet, MatToolbar, DummyComponent, RouterLink, RouterLinkActive, MatIcon],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/login/authentication.service.ts
+++ src/app/login/authentication.service.ts
@@ -1,16 +1,29 @@
 import { Injectable } from "@angular/core";
 import { User } from "./model/user";
 
+const USER_STORAGE_KEY = "angular-crm.user";
 @Injectable({ providedIn: "root" })
 export class AuthenticationService {
-  constructor() {}
+  private currentUser?: User;
+  constructor() {
+    // Check user connected?
+    if (sessionStorage.getItem(USER_STORAGE_KEY) !== null) {
+      this.currentUser = JSON.parse(sessionStorage.getItem(USER_STORAGE_KEY)!);
+    }
+  }
+
+  public get authenticated(): boolean {
+    return !!this.currentUser;
+  }
 
   authentUser(login: string, password: string): User {
-    return {
+    this.currentUser = {
       id: 1,
       login: login,
       lastname: "Doe",
       firstname: "John",
     };
+    sessionStorage.setItem(USER_STORAGE_KEY, JSON.stringify(this.currentUser));
+    return this.currentUser;
   }
 }
